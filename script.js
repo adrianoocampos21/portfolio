73ec60c6244645757d64374c06b082cb14f3ff91
Adriano Ocampos <161398987+adrianoocampos21@users.noreply.github.com>
2025-03-28 17:08:42 -0300

    update
    Formulario entregando dados reais

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const form = document.getElementById("contato");
+const nome = document.getElementById("nome");
+const email = document.getElementById('email');
+const mensagem = document.getElementById('mensagem');
+
+
