bac3f892289d68dfc6ab008024b6a2fe59191686
Adriano Ocampos <161398987+adrianoocampos21@users.noreply.github.com>
2025-04-08 17:14:59 -0300

    update
    atualização no código, responsividade no menu ao reduzir para 700px e inserção de imagens

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-
-const form = document.getElementById("contato");
-const nome = document.getElementById("nome");
-const email = document.getElementById('email');
-const mensagem = document.getElementById('mensagem');
-
-
+getElementById('saibamais');
