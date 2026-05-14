fb30eb4f0356a19bd5cd9a658ac1207802b8b801
Adriano Ocampos <161398987+adrianoocampos21@users.noreply.github.com>
2026-05-14 15:46:59 -0300

    atualizações
    opção para mudar o tema e botão para voltar ao topo da página

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
+const btnTheme = document.getElementById('troca-tema');
 
+btnTheme.addEventListener('click', () => {
+    // O 'toggle' adiciona a classe se ela não existir e remove se já existir
+    document.body.classList.toggle('dark-theme');
+    
+    // Opcional: Salvar a preferência do usuário no navegador
+    const isDark = document.body.classList.contains('dark-theme');
+    localStorage.setItem('theme', isDark ? 'dark' : 'light');
+});
+
+// Ao carregar a página, verifica se o usuário já tinha escolhido o tema escuro
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark-theme');
+}
 
 //baixar curriculo
 document.getElementById('btn-download').addEventListener('click', function() {
@@ -11,6 +25,26 @@ document.getElementById('btn-download').addEventListener('click', function() {
     } else {
         // Se preenchido, redireciona para o arquivo do currículo
         // Substitua pelo caminho real do seu arquivo PDF
-        window.location.href = 'caminho/do/seu/curriculo.pdf';
+        window.location.href = 'media/Adriano_Analista_TI_curriculo.pdf';
     }
+});
+
+
+const btnTop = document.getElementById('back-to-top');
+
+// 1. Mostrar/Esconder o botão conforme o scroll
+window.onscroll = function() {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+        btnTop.style.display = "block";
+    } else {
+        btnTop.style.display = "none";
+    }
+};
+
+// 2. Função de clique para voltar ao topo
+btnTop.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Scroll suave
+    });
 });
